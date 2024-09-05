0ea443c7cbd029e9c3892d88f0341ccc4d2d966a
BHyeonKim <rlaqhguse@gmail.com>
2024-09-06 01:18:28 +0900

    chore: eslint 및 prettier 설치 및 설정 추가

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "printWidth": 120,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "bracketSameLine": true,
+  "semicolons": true,
+  "bracketLine": false,
+  "endOfLine": "auto",
+}
