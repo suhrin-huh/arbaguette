e26aacd027e23da8b7a5666d920efb37f7c997db
BHyeonKim <rlaqhguse@gmail.com>
2024-09-06 10:38:10 +0900

    feat: 프리텐다드 폰트 로드

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,10 @@
 import 'react-native-reanimated';
 
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+
+import usePretendardFonts from '@/hooks/usePretendardFonts';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -20,10 +20,7 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+  const [loaded, error] = usePretendardFonts();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
