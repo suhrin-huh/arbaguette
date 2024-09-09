34de8d6582f5ec425a5e41986eca56e6e76c7200
BHyeonKim <rlaqhguse@gmail.com>
2024-09-09 18:01:08 +0900

    chore: 폴더 구조 변경

--- frontend/app/(app)/_layout.tsx
+++ frontend/app/(app)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 const AppLayout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }} initialRouteName="public">
-      <Stack.Screen name="(public)" />
+      <Stack.Screen name="public" />
       <Stack.Screen name="boss" />
       <Stack.Screen name="crew" />
     </Stack>
