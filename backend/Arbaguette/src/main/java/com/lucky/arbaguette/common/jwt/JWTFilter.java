6072c697ab3041b09e4218e041db000c206b9594
박지훈 <pjh2996@naver.com>
2024-09-09 17:57:30 +0900

    [BE] feat: jwt 설정 추가

---
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/jwt/JWTFilter.java
@@ -0,0 +1,62 @@
+package com.lucky.arbaguette.common.jwt;
+
+import com.lucky.arbaguette.common.domain.dto.CommonUserInfo;
+import com.lucky.arbaguette.common.domain.dto.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        //request 에서 Authorization 헤더를 찾음
+        String authorization = request.getHeader("Authorization");
+
+        //Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        //토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+            System.out.println("token is expired");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String email = jwtUtil.getEmail(token);
+        String role = jwtUtil.getRole(token);
+
+        CommonUserInfo userInfo = CommonUserInfo.builder()
+                .email(email)
+                .role(role)
+                .build();
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(userInfo);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
+                customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
