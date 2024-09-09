5e6b26ac5864885417eb9cc0d8388158cf9543ab
박지훈 <pjh2996@naver.com>
2024-09-09 23:12:02 +0900

    [BE] refactor: jpa 상속으로 변경

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/crew/repository/CrewRepository.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/crew/repository/CrewRepository.java
@@ -2,9 +2,12 @@ package com.lucky.arbaguette.crew.repository;
 
 import com.lucky.arbaguette.crew.domain.Crew;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CrewRepository extends CrudRepository<Crew, Integer> {
+public interface CrewRepository extends JpaRepository<Crew, Integer> {
+
+    public boolean existsByEmail(String email);
 
     Optional<Crew> findByEmail(String email);
+
 }
