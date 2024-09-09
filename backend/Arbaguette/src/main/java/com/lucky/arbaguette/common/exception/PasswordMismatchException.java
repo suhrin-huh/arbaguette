842ead5a1615587f5a5dd1dd9c151ce94ffd01fa
김지원 <gokim0928@naver.com>
2024-09-09 16:57:06 +0900

    [BE] fix: 아이디 중복 확인 예외 수정 (#8)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/exception/PasswordMismatchException.java
+++
@@ -1,7 +0,0 @@
-package com.lucky.arbaguette.common.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
