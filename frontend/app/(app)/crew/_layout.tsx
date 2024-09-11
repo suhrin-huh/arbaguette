921c19b8faa52043e984bb44e4a0759437010dea
BHyeonKim <rlaqhguse@gmail.com>
2024-09-11 10:40:58 +0900

    chore: 알바생 라우트 구조 변경

--- frontend/app/(app)/crew/_layout.tsx
+++ frontend/app/(app)/crew/_layout.tsx
@@ -1,11 +1,13 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 
 const CrewRootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="main" />
-    </Stack>
+    <Tabs initialRouteName="index" screenOptions={{ headerStyle: { backgroundColor: 'transparent', elevation: 0 } }}>
+      <Tabs.Screen name="index" options={{ title: '홈' }} />
+      <Tabs.Screen name="schedule" options={{ title: '스케줄' }} />
+      <Tabs.Screen name="management" options={{ title: '직원 관리' }} />
+      <Tabs.Screen name="banking" options={{ title: '뱅킹' }} />
+    </Tabs>
   );
 };
 
