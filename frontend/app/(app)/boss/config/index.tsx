30a4c978bfbe7daaaf5007e0e7ac0c20a8df22c1
BHyeonKim <rlaqhguse@gmail.com>
2024-09-19 16:39:39 +0900

    [FE] refactor: NFC 등록 모달 리펙토링

--- frontend/app/(app)/boss/config/index.tsx
+++ frontend/app/(app)/boss/config/index.tsx
@@ -1,11 +1,7 @@
 import styled from '@emotion/native';
-import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
-import LottieView from 'lottie-react-native';
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { router } from 'expo-router';
+import { useState } from 'react';
 
-import NfcAnimation from '@/assets/lottie/nfc_ready.json';
-import CustomBackdrop from '@/components/common/BottomSheetOption/CustomBackdrop';
-import CustomBackground from '@/components/common/BottomSheetOption/CustomBackgound';
 import StoreCard from '@/components/common/StoreCard';
 import Layout from '@/constants/Layout';
 
@@ -26,21 +22,13 @@ const ConfigScreen = () => {
     { storeId: 6, name: '파리파게트 장덕점', address: '광주광역시 동일로 11-6' },
   ]);
 
-  const animation = useRef<LottieView>(null);
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const snapPoints = useMemo(() => ['40%', '40%'], []);
-
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
-
   const pressStoreCard = (card: willStoreDto) => {
     console.log('카드의 ID : ', card);
   };
 
   const pressNfcButton = (card: willStoreDto) => {
     console.log('NFC 버튼 눌림 : ', card.storeId);
-    handlePresentModalPress();
+    router.push('/(app)/boss/config/modal');
   };
 
   const addStoreHandler = () => {
@@ -61,39 +49,16 @@ const ConfigScreen = () => {
         <StoreCard storeData={store} onPress={pressStoreCard} key={store.storeId} onPressNFC={pressNfcButton} />
       ))}
       <StoreCard storeData="ADD" onPress={addStoreHandler} />
-      <BottomSheetModal
-        ref={bottomSheetModalRef}
-        index={1}
-        snapPoints={snapPoints}
-        backgroundComponent={CustomBackground}
-        backdropComponent={CustomBackdrop}>
-        <BottomSheetViewArea>
-          <NfcReadyTextBox>
-            <NfcReadyText1>출근 관리용 NFC를</NfcReadyText1>
-            <NfcReadyText2>등록해주세요.</NfcReadyText2>
-          </NfcReadyTextBox>
-          <LottieView
-            autoPlay
-            ref={animation}
-            speed={1.5}
-            style={{
-              width: 280,
-              height: 280,
-            }}
-            source={NfcAnimation}
-          />
-        </BottomSheetViewArea>
-      </BottomSheetModal>
     </InnerContainer>
   );
 };
 
 export default ConfigScreen;
 
-const InnerContainer = styled.ScrollView(({ theme }) => ({
+const InnerContainer = styled.ScrollView({
   flexGrow: 1,
   overflow: 'visible',
-}));
+});
 
 const ConfigTitle = styled.Text(({ theme }) => ({
   fontSize: 28,
@@ -102,26 +67,4 @@ const ConfigTitle = styled.Text(({ theme }) => ({
   textAlign: 'left',
   marginTop: 80 - theme.layout.PADDING.VERTICAL,
   marginBottom: 30,
-}));
-
-const BottomSheetViewArea = styled(BottomSheetView)(({ theme }) => ({
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-}));
-
-const NfcReadyTextBox = styled.View(({ theme }) => ({
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginTop: 30,
-}));
-const NfcReadyText1 = styled.Text(({ theme }) => ({
-  fontSize: 24,
-  color: 'white',
-  fontWeight: 600,
-}));
-const NfcReadyText2 = styled.Text(({ theme }) => ({
-  fontSize: 24,
-  color: 'white',
-  fontWeight: 600,
 }));
--- frontend/components/common/BottomSheetOption/CustomBackdrop.tsx
+++ frontend/components/common/BottomSheetOption/CustomBackdrop.tsx
@@ -1,26 +1,20 @@
 import type { BottomSheetBackdropProps } from '@gorhom/bottom-sheet';
-import React, { useMemo } from 'react';
-import Animated, { Extrapolate, interpolate, useAnimatedStyle } from 'react-native-reanimated';
+import React from 'react';
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 
 const CustomBackdrop = ({ animatedIndex, style }: BottomSheetBackdropProps) => {
-  // animated variables
-  const containerAnimatedStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(animatedIndex.value, [0, 1], [0, 0.3], Extrapolate.CLAMP),
-  }));
-
-  // styles
-  const containerStyle = useMemo(
-    () => [
-      style,
-      {
-        backgroundColor: '#000000',
-      },
-      containerAnimatedStyle,
-    ],
-    [style, containerAnimatedStyle],
+  return (
+    <Animated.View
+      style={[
+        style,
+        {
+          backgroundColor: 'rgba(0,0,0,0.3)',
+        },
+      ]}
+      entering={FadeIn}
+      exiting={FadeOut}
+    />
   );
-
-  return <Animated.View style={containerStyle} />;
 };
 
 export default CustomBackdrop;
