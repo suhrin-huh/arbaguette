fe80fdaf4e5177331d475acad76673add7c820a0
박지훈 <pjh2996@naver.com>
2024-09-21 01:08:42 +0900

    [BE] fix: 시간 반환 타입 및 형태 수정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/dto/response/ScheduleSaveResponse.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/dto/response/ScheduleSaveResponse.java
@@ -1,11 +1,12 @@
 package com.lucky.arbaguette.schedule.dto.response;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record ScheduleSaveResponse(String workStatus,
-                                   LocalDateTime time) {
+                                   String time) {
 
     public static ScheduleSaveResponse from(String workStatus, LocalDateTime time) {
-        return new ScheduleSaveResponse(workStatus, time);
+        return new ScheduleSaveResponse(workStatus, time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 }
