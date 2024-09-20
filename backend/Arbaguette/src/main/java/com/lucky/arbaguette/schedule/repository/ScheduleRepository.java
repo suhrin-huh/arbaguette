4354a5250e4263b380ddcd4b04ce19079e13774f
박지훈 <pjh2996@naver.com>
2024-09-20 16:24:36 +0900

    [BE] fix: 중복 메서드 수정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/repository/ScheduleRepository.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/repository/ScheduleRepository.java
@@ -10,19 +10,18 @@ import org.springframework.data.repository.query.Param;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
 
-    @Query("SELECT s FROM Schedule s WHERE s.crew = :crewId AND s.startTime BETWEEN :startDate AND :endDate")
-    List<Schedule> findScheduleByCrewAndMonth(@Param("crewId") int crewId, @Param("startDate") LocalDateTime startDate,
-                                              @Param("endDate") LocalDateTime endDate);
-
     @Query("SELECT s FROM Schedule s WHERE s.crew.crewId = :crewId AND "
             + "(FUNCTION('DATE_FORMAT', s.startTime, '%m%d') = FUNCTION('DATE_FORMAT', :nowTime, '%m%d') OR "
             + "FUNCTION('DATE_FORMAT', s.endTime, '%m%d') = FUNCTION('DATE_FORMAT', :nowTime, '%m%d')) ")
     Optional<Schedule> findByCrewAndTime(int crewId, LocalDateTime nowTime);
 
     @Query("SELECT s FROM Schedule s WHERE s.crew.crewId = :crewId AND s.status = 'YES' AND s.startTime BETWEEN :startDate AND :endDate ")
-    List<Schedule> findScheduleByCrewAndMonth(@Param("crewId") int crewId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+    List<Schedule> findScheduleByCrewAndMonth(@Param("crewId") int crewId, @Param("startDate") LocalDateTime startDate,
+                                              @Param("endDate") LocalDateTime endDate);
 
     @Query("SELECT s FROM Schedule s WHERE s.crew.crewId = :crewId AND s.startTime BETWEEN :startDate AND :endDate ")
-    List<Schedule> findAllScheduleByCrewAndMonth(@Param("crewId") int crewId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+    List<Schedule> findAllScheduleByCrewAndMonth(@Param("crewId") int crewId,
+                                                 @Param("startDate") LocalDateTime startDate,
+                                                 @Param("endDate") LocalDateTime endDate);
 
 }
