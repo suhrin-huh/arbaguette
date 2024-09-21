0670e230969d8dc9bdb87d781d0e67ea078df9c0
monam2 <kangcw0107@gmail.com>
2024-09-21 20:40:39 +0900

    [FE] feat: 사업자 등록증 카메라 페이지 라우트 추가(#28)

--- frontend/app/(app)/boss/config/register/upload/_layout.tsx
+++ frontend/app/(app)/boss/config/register/upload/_layout.tsx
@@ -2,8 +2,9 @@ import { Stack } from 'expo-router';
 
 const UploadLayout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }} initialRouteName="upload">
+    <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
       <Stack.Screen name="index" />
+      <Stack.Screen name="camera" />
     </Stack>
   );
 };
