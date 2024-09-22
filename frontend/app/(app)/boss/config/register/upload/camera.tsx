54cca02fc81398c9bfbcd2f598a071bc80252c4e
monam2 <kangcw0107@gmail.com>
2024-09-22 18:08:57 +0900

    [FE] refactor: 사업자 등록증 촬영 함수 분리(#41)

--- frontend/app/(app)/boss/config/register/upload/camera.tsx
+++ frontend/app/(app)/boss/config/register/upload/camera.tsx
@@ -1,42 +1,26 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useRef } from 'react'
+import { View } from 'react-native'
+import React, { useRef } from 'react'
 import styled from '@emotion/native'
 import Button from '@/components/common/Button'
 import { CameraView, useCameraPermissions } from 'expo-camera'
 import { FontAwesome5 } from '@expo/vector-icons'
 import { useCertifiedPaperStore } from '@/zustand/boss/useCertifiedPaperStore'
 import { router } from 'expo-router'
+import takePictureToFormData from '@/util/boss/takePictureToFormData'
 
 const CameraScreen = () => {
   const [permission, requestPermission] = useCameraPermissions();
   const cameraRef = useRef<CameraView | null>(null);
-
   const { setCertifiedPaper, setPaperUri } = useCertifiedPaperStore();
 
   const takePicture = async () => {
-    try {
-      if (cameraRef.current) {
-        const newPhoto = await cameraRef.current.takePictureAsync({
-          quality: 0.5,
-          base64: true,
-          exif: true,
-        });
-        if (newPhoto) {
-          const formData = new FormData();
-          formData.append('image', {
-            uri: newPhoto.uri,
-            type: 'image/jpeg',
-            name: `photo_${Date.now()}.jpeg`
-          } as any);
+    const result = await takePictureToFormData(cameraRef);
 
-          setCertifiedPaper(formData);
-          setPaperUri(newPhoto.uri);
-          router.push('./check');
-        }
-
-      }
-    } catch (error) {
-      console.error("Error taking picture: ", error);
+    if (result) {
+      const { formData, uri } = result;
+      setCertifiedPaper(formData);
+      setPaperUri(uri);
+      router.push('./check');
     }
   };
 
@@ -48,7 +32,7 @@ const CameraScreen = () => {
     return (
       <PermissionContainer>
         <PermissionText>카메라 사용 권한이 필요합니다</PermissionText>
-        <Button onPress={requestPermission}>
+        <Button type='primary' onPress={requestPermission}>
           권한 요청
         </Button>
       </PermissionContainer>
