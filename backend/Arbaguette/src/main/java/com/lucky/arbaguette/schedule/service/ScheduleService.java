a290fdbf10d471dab97829eeece8983a8f5cce94
박지훈 <pjh2996@naver.com>
2024-09-22 20:32:34 +0900

    [BE] refactor: 출/퇴근 중복 에러 수정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/service/ScheduleService.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/schedule/service/ScheduleService.java
@@ -2,6 +2,7 @@ package com.lucky.arbaguette.schedule.service;
 
 import com.lucky.arbaguette.common.domain.dto.CustomUserDetails;
 import com.lucky.arbaguette.common.exception.BadRequestException;
+import com.lucky.arbaguette.common.exception.DuplicateException;
 import com.lucky.arbaguette.common.exception.NotFoundException;
 import com.lucky.arbaguette.crew.domain.Crew;
 import com.lucky.arbaguette.crew.repository.CrewRepository;
@@ -27,10 +28,10 @@ public class ScheduleService {
                 .orElseThrow(() -> new NotFoundException("알바생을 찾을 수 없습니다."));
 
         Schedule schedule = scheduleRepository.findByCrewAndTime(crew.getCrewId(), nowTime)
-                .orElseThrow(() -> new NotFoundException("출근 날짜가 아닙니다."));
+                .orElseThrow(() -> new BadRequestException("출근 날짜가 아닙니다."));
 
         if (schedule.isAlreadyOutWork()) {
-            throw new BadRequestException("이미 퇴근처리 되었습니다.");
+            throw new DuplicateException("이미 퇴근처리 되었습니다.");
         }
 
         if (schedule.isBeforeWork()) {
