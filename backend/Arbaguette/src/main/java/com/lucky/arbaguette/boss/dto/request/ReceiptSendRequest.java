d9c5acf246898db81262da1e897f6f095a0b3eb2
손다인 <dain0826@naver.com>
2024-09-24 05:08:53 +0900

    [BE] chore: receipt 오타 수정 (#45)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/boss/dto/request/ReceiptSendRequest.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/boss/dto/request/ReceiptSendRequest.java
@@ -1,7 +1,7 @@
 package com.lucky.arbaguette.boss.dto.request;
 
-public record ReciptSendRequest(int month,
-                                int originSalary,
-                                int totalTime,
-                                String crewEmail) {
+public record ReceiptSendRequest(int month,
+                                 int originSalary,
+                                 int totalTime,
+                                 String crewEmail) {
 }
