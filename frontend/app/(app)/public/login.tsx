7f335f10fecde887a51717eeebe684d312fffc85
황민채 <trick0846@naver.com>
2024-09-23 11:40:08 +0900

    [FE] feat: 로그인페이지 API 연결 및 error 설정

--- frontend/app/(app)/public/login.tsx
+++ frontend/app/(app)/public/login.tsx
@@ -1,4 +1,6 @@
 import Styled from '@emotion/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { AxiosError } from 'axios';
 import { router } from 'expo-router';
 import { useState } from 'react';
 import type { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
@@ -29,9 +31,17 @@ const StyledTitle = Styled.Text<{ isHeader?: boolean }>(({ isHeader }) => ({
   fontWeight: 'bold',
 }));
 
-const LoginScreen = (): JSX.Element => {
+const ErrorText = Styled.Text(() => ({
+  color: 'red',
+  fontSize: 16,
+  marginTop: 10,
+}));
+
+const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleEmailInput = (e: NativeSyntheticEvent<TextInputChangeEventData>): void => {
     setEmail(e.nativeEvent.text);
@@ -49,25 +59,56 @@ const LoginScreen = (): JSX.Element => {
     setPassword('');
   };
 
+  interface ResponseData {
+    accessToken: string;
+    role: string;
+  }
+
+  interface Response {
+    code: number;
+    data: ResponseData;
+  }
+
+  interface ErrorResponse {
+    code: number;
+    message: string;
+  }
+
   const handleLogin = async (): Promise<void> => {
     try {
-      const response = await instance.post(
-        '/login',
+      // Post request to login endpoint
+      const response = await instance.post<Response>(
+        '/api/login',
         {
           email,
           password,
         },
         { headers: { 'Content-Type': 'application/json' } },
       );
-      console.log(response);
+
       if (response.data.code === 200) {
-        router.push('/(app)/public');
+        const { accessToken, role } = response.data.data;
+
+        await AsyncStorage.setItem('accessToken', accessToken);
+
+        if (role === 'BOSS') {
+          router.push('/(app)/boss');
+        } else if (role === 'CREW') {
+          router.push('/(app)/crew');
+        }
       }
     } catch (error) {
-      console.log(error);
+      const axiosError = error as AxiosError<ErrorResponse>;
+
+      const err = axiosError.response?.data;
+      if (err && err.code === 404) {
+        setIsValid(false);
+        setErrorMessage('아이디와 비밀번호를 확인해주세요.');
+      } else {
+        console.error('An error occurred:', error);
+      }
     }
   };
-
   return (
     <Container>
       <ContentWrapper>
@@ -76,6 +117,7 @@ const LoginScreen = (): JSX.Element => {
           <LabeledInput
             label="이메일"
             value={email}
+            isValid={isValid}
             placeholder="이메일을 입력하세요"
             onChange={handleEmailInput}
             handleDeleteText={clearEmailInput}
@@ -86,12 +128,15 @@ const LoginScreen = (): JSX.Element => {
           <LabeledInput
             label="비밀번호"
             value={password}
+            isValid={isValid}
             placeholder="비밀번호를 입력하세요"
             onChange={handlePasswordInput}
             handleDeleteText={clearPasswordInput}
             enableDeleteButton={true}
+            secureTextEntry={true}
           />
         </InputWrapper>
+        {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
       </ContentWrapper>
       <Button type="primary" onPress={handleLogin}>
         로그인
