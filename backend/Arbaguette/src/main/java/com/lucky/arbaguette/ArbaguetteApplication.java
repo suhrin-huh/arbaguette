a3b381f0685181be2a674ee427c9d4e0e776b9c6
손다인 <dain0826@naver.com>
2024-09-23 14:36:42 +0900

    [BE] config: env 설정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/ArbaguetteApplication.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/ArbaguetteApplication.java
@@ -1,6 +1,5 @@
 package com.lucky.arbaguette;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +10,6 @@ public class ArbaguetteApplication {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
-        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("SSAFY_BANK_KEY", dotenv.get("SSAFY_BANK_KEY"));
         SpringApplication.run(ArbaguetteApplication.class, args);
     }
 
