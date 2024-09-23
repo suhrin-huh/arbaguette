117bf25c7b9ae35bb33076203fffb5fdeb7e523a
김지원 <gokim0928@naver.com>
2024-09-23 17:17:43 +0900

    [BE] feat: 회원 상세 조회 구현 (#51)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/boss/controller/BossController.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/boss/controller/BossController.java
@@ -5,6 +5,7 @@ import com.lucky.arbaguette.boss.dto.ReciptSendRequest;
 import com.lucky.arbaguette.boss.service.BossService;
 import com.lucky.arbaguette.common.ApiResponse;
 import com.lucky.arbaguette.common.domain.dto.CustomUserDetails;
+import com.lucky.arbaguette.crew.domain.dto.response.CrewDetailResponse;
 import com.lucky.arbaguette.crew.domain.dto.response.CrewListResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,15 +20,20 @@ public class BossController {
 
     private final BossService bossService;
 
+    @PostMapping("/crew")
+    public ApiResponse<Void> saveCrew(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CrewSaveRequest crewSaveRequest) {
+        bossService.saveCrew(customUserDetails, crewSaveRequest);
+        return ApiResponse.success();
+    }
+
     @GetMapping("/crews") // 알바생 전체 목록 조회
     public ApiResponse<CrewListResponse> getCrews(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("companyId") int companyId) {
         return ApiResponse.success(bossService.getCrews(customUserDetails, companyId));
     }
 
-    @PostMapping("/crew")
-    public ApiResponse<Void> saveCrew(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CrewSaveRequest crewSaveRequest) {
-        bossService.saveCrew(customUserDetails, crewSaveRequest);
-        return ApiResponse.success();
+    @GetMapping("/crew")
+    public ApiResponse<CrewDetailResponse> getCrewDetails(@AuthenticationPrincipal CustomUserDetails cutstomUserDetails, @RequestParam int crewId) {
+        return ApiResponse.success(bossService.getCrewDetails(cutstomUserDetails, crewId));
     }
 
     @PostMapping("/receipt")
