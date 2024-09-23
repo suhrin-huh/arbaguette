c3cd1b59886ad1875e42b6a8a3e43034872087da
손다인 <dain0826@naver.com>
2024-09-24 05:18:52 +0900

    [BE] feat: 근무계약일 빌더 추가 (#45)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/contractworkingday/domain/ContractWorkingDay.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/contractworkingday/domain/ContractWorkingDay.java
@@ -10,6 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import java.time.LocalTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
@@ -34,5 +35,13 @@ public class ContractWorkingDay {
 
     @JsonFormat(pattern = "HH:mm")
     private LocalTime endTime;
+
+    @Builder
+    public ContractWorkingDay(Contract contract, LocalTime startTime, LocalTime endTime, int weekday){
+        this.id = new ContractWorkingDayId(contract.getContractId(), weekday);
+        this.contract = contract;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
 }
