22e078e2c81014196eadd8acfa9ba144f08bf9af
손다인 <dain0826@naver.com>
2024-09-24 05:12:00 +0900

    [BE] feat: 근로계약서 작성 request dto 추가 (#45)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/contract/domain/dto/ContractSaveRequest.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/contract/domain/dto/ContractSaveRequest.java
@@ -1,21 +1,37 @@
 package com.lucky.arbaguette.contract.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lucky.arbaguette.company.domain.Company;
+import com.lucky.arbaguette.contract.domain.Contract;
 import com.lucky.arbaguette.contract.domain.TaxType;
 import com.lucky.arbaguette.contractworkingday.domain.dto.WorkingDayInfo;
-import org.springframework.web.multipart.MultipartFile;
+import com.lucky.arbaguette.crew.domain.Crew;
 
-import java.time.LocalDateTime;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public record ContractSaveRequest(int companyId,
-                                  String name,
-                                  String tel,
-                                  @JsonFormat(pattern = "yyyy-MM-dd")LocalDateTime startDate,
-                                  @JsonFormat(pattern = "yyyy-MM-dd")LocalDateTime endDate,
+                                  int crewId,
+                                  @JsonFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+                                  @JsonFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
                                   List<WorkingDayInfo> workingDayInfoList,
                                   int salary,
-                                  TaxType taxType,
-                                  MultipartFile multipartFile
+                                  int salaryDate,
+                                  TaxType taxType
                                   ) {
+
+    public Contract toContract(Company company, Crew crew, String url) throws IOException {
+        return Contract.builder()
+                .company(company)
+                .crew(crew)
+                .startDate(this.startDate)
+                .endDate(this.endDate)
+                .salary(this.salary)
+                .salaryDate(this.salaryDate)
+                .tax(this.taxType)
+                .bossSign(url)
+                .build();
+    }
+
 }
