36d007db40bad331e72f6e11d09361ff4f0b8def
손다인 <dain0826@naver.com>
2024-09-24 05:19:32 +0900

    [BE] feat: 근무계약일 중복조회 메서드 추가 (#45)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/contractworkingday/repository/ContractWorkingDayRepository.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/contractworkingday/repository/ContractWorkingDayRepository.java
@@ -2,6 +2,7 @@ package com.lucky.arbaguette.contractworkingday.repository;
 
 import com.lucky.arbaguette.contract.domain.Contract;
 import com.lucky.arbaguette.contractworkingday.domain.ContractWorkingDay;
+import com.lucky.arbaguette.contractworkingday.domain.ContractWorkingDayId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 public interface ContractWorkingDayRepository extends JpaRepository<ContractWorkingDay, Integer> {
 
     List<ContractWorkingDay> findAllByContract(Contract contract);
+
+    boolean existsByContractAndId_Weekday(Contract contract, int weekday);
 
 }
