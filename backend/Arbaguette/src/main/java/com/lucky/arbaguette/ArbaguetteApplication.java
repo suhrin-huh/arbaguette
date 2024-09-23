be87c8848772c6d9c53dda6b376e21bb6d3ca866
김지원 <gokim0928@naver.com>
2024-09-23 14:06:16 +0900

    [BE] fix: env lcoal 수정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/ArbaguetteApplication.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/ArbaguetteApplication.java
@@ -1,14 +1,22 @@
 package com.lucky.arbaguette;
 
-import java.util.TimeZone;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ArbaguetteApplication {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
+        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("SSAFY_BANK_KEY", dotenv.get("SSAFY_BANK_KEY"));
         SpringApplication.run(ArbaguetteApplication.class, args);
     }
 
