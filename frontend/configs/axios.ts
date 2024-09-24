3eae5a18efed4c6f68f751838c2ee94d94a35c15
황민채 <trick0846@naver.com>
2024-09-24 11:21:19 +0900

    [FE] refactor: 오류확인 코드 제거

--- frontend/configs/axios.ts
+++ frontend/configs/axios.ts
@@ -10,7 +10,6 @@ const instance = axios.create({
 instance.interceptors.request.use(
   async (config) => {
     const token = await AsyncStorage.getItem('accessToken');
-    console.log('토큰 확인하기', token);
     if (token !== null) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
@@ -26,16 +25,9 @@ instance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    if (error.response.status === 401 && error.response.data.message === '로그인이 필요합니다.') {
-      console.log('401 error');
-      console.log(error.response.data);
-      // 401 뜬다 => accesstoken이 만료? => 갱신 후 다시 재요청
-      // 그게 아니라면? (로그인 페이지로 이동이 필요?)
-
-      // 그 외에는 그냥 error 반환
-    }
+    // if (error.response.status === 401 && error.response.data.message === '로그인이 필요합니다.') {
+    // }
     return Promise.reject(error);
-    // console.log('response inter:', error.response.status);
   },
 );
 
