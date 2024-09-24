c7734a857232da65751215bc60e477c96e0daebd
김지원 <gokim0928@naver.com>
2024-09-24 15:48:29 +0900

    [BE] feat: 알바생 급여 개별 송금 구현 (#59)

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/controller/BankController.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/controller/BankController.java
@@ -3,6 +3,7 @@ package com.lucky.arbaguette.common.controller;
 import com.lucky.arbaguette.common.ApiResponse;
 import com.lucky.arbaguette.common.domain.dto.CustomUserDetails;
 import com.lucky.arbaguette.common.domain.dto.request.SendMoneyRequest;
+import com.lucky.arbaguette.common.domain.dto.request.SendSalaryRequest;
 import com.lucky.arbaguette.common.domain.dto.response.AccountResponse;
 import com.lucky.arbaguette.common.service.BankService;
 import lombok.RequiredArgsConstructor;
@@ -25,14 +26,21 @@ public class BankController {
         return ApiResponse.success(bankService.getAccount(customUserDetails));
     }
 
+    @GetMapping("/history")
+    public ApiResponse<Map<String, Object>> getHistory(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ApiResponse.success(bankService.getHistory(customUserDetails));
+    }
+
     @PostMapping("/remittance")
     public ApiResponse<Void> sendMoney(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody SendMoneyRequest request) {
         bankService.sendMoney(customUserDetails, request);
         return ApiResponse.success();
     }
 
-    @GetMapping("/history")
-    public ApiResponse<Map<String, Object>> getHistory(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        return ApiResponse.success(bankService.getHistory(customUserDetails));
+    @PostMapping("/remittance/salary")
+    public ApiResponse<Void> sendSalary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody SendSalaryRequest request) {
+        bankService.sendSalary(customUserDetails, request);
+        return ApiResponse.success();
     }
+
 }
