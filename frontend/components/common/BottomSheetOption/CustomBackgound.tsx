e57b74eaf3854fc435901c7394fac6aa2e2ed12c
BHyeonKim <rlaqhguse@gmail.com>
2024-09-24 13:54:42 +0900

    [FE] refactor: 불필요코드 삭제

--- frontend/components/common/BottomSheetOption/CustomBackgound.tsx
+++ frontend/components/common/BottomSheetOption/CustomBackgound.tsx
@@ -1,17 +1,15 @@
+import Styled from '@emotion/native';
 import type { BottomSheetBackgroundProps } from '@gorhom/bottom-sheet';
 import React from 'react';
-import Animated, { interpolateColor, useAnimatedStyle } from 'react-native-reanimated';
 
-import Colors from '@/constants/Colors';
+const Background = Styled.View(({ theme }) => ({
+  backgroundColor: theme.color.PRIMARY,
+  borderRadius: 24,
+  elevation: 4,
+}));
 
-const CustomBackground: React.FC<BottomSheetBackgroundProps> = ({ style, animatedIndex }) => {
-  const containerAnimatedStyle = useAnimatedStyle(() => ({
-    backgroundColor: interpolateColor(animatedIndex.value, [0, 1], [Colors.PRIMARY, Colors.PRIMARY]),
-    borderRadius: 24,
-    elevation: 4,
-  }));
-
-  return <Animated.View pointerEvents="none" style={[style, containerAnimatedStyle]} />;
+const CustomBackground: React.FC<BottomSheetBackgroundProps> = ({ style }) => {
+  return <Background pointerEvents="none" style={style} />;
 };
 
 export default CustomBackground;
