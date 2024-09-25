f93261cb6d6f2078de3a4a3507dafbd82f51d60c
BHyeonKim <rlaqhguse@gmail.com>
2024-09-25 15:27:16 +0900

    [FE] style: 알바 화면 레이아웃 수정 (#67)

--- frontend/app/(app)/_layout.tsx
+++ frontend/app/(app)/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from 'expo-router';
 
+import Theme from '@/styles/Theme';
+
 const AppLayout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }} initialRouteName="public">
       <Stack.Screen name="public" />
       <Stack.Screen name="boss" />
-      <Stack.Screen name="crew" />
+      <Stack.Screen name="crew" options={{ contentStyle: { paddingHorizontal: Theme.layout.PADDING.HORIZONTAL } }} />
     </Stack>
   );
 };
--- frontend/app/(app)/crew/_layout.tsx
+++ frontend/app/(app)/crew/_layout.tsx
@@ -1,11 +1,30 @@
-import { Tabs } from 'expo-router';
+import { router, Tabs } from 'expo-router';
+
+import BellButton from '@/components/crew/BellButton';
+import CalendarButton from '@/components/crew/CalendarButton';
 
 const CrewMainLayout = () => {
   return (
-    <Tabs initialRouteName="main" screenOptions={{ headerStyle: { backgroundColor: 'transparent', elevation: 0 } }}>
+    <Tabs
+      initialRouteName="main"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+        },
+        headerTitleAlign: 'center',
+      }}>
       <Tabs.Screen name="main" options={{ title: '홈' }} />
       <Tabs.Screen name="schedule" options={{ title: '스케줄' }} />
-      <Tabs.Screen name="management" options={{ title: '직원 관리' }} />
+      <Tabs.Screen
+        name="management"
+        options={{
+          title: '근무 내역',
+          headerTitle: '근무 내역 조회',
+          headerRight: () => <BellButton onPress={() => router.navigate('/crew/management/contract')} />,
+          headerLeft: () => <CalendarButton onPress={() => router.navigate('/crew/management/contract')} />,
+        }}
+      />
       <Tabs.Screen name="banking" options={{ title: '뱅킹' }} />
     </Tabs>
   );
