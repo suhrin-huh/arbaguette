f80acd5cfc2ef05dff5161b03d3392bbfee65081
황민채 <trick0846@naver.com>
2024-09-25 11:42:04 +0900

    [FE] feat: decode token 구현

---
+++ frontend/util/common/decodeToken.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { JwtPayload } from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+
+export interface Decode extends JwtPayload {
+  role: 'BOSS' | 'CREW';
+  string: string;
+}
+
+const decodeToken = async () => {
+  const token = await AsyncStorage.getItem('accessToken');
+  if (!token) {
+    return null;
+  }
+  const decoded = jwtDecode<Decode>(token);
+  console.log(decoded);
+  return decoded;
+};
+
+export default decodeToken;
