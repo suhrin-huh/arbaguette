1318b6b738a82f8415b726638f1555e9ff337941
황민채 <trick0846@naver.com>
2024-09-27 15:59:17 +0900

    Revert "[FE] chore: 스타일관련 상수(폰트) 추가"

--- frontend/styles/Theme.tsx
+++ frontend/styles/Theme.tsx
@@ -1,13 +1,11 @@
 import Colors from '@/constants/Colors';
 import Dimensions from '@/constants/Dimensions';
-import Font from '@/constants/Font';
 import Layout from '@/constants/Layout';
 
 const theme = {
   color: { ...Colors },
   dimensions: { ...Dimensions },
   layout: Layout,
-  font: { ...Font },
 };
 
 export default theme;
