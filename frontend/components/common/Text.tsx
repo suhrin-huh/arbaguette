f1e30905ca77c4daeb451fc11209af4e56b8b2c7
황민채 <trick0846@naver.com>
2024-09-27 15:48:09 +0900

    [FE] chore: 스타일관련 상수(폰트) 추가

---
+++ frontend/components/common/Text.tsx
@@ -0,0 +1,48 @@
+import Styled from '@emotion/native';
+import type { StyleProp, TextProps, TextStyle } from 'react-native';
+import { Text as RNText } from 'react-native';
+
+type TextSize = 'base' | 'sub' | 'title';
+type TextWeight = 'bold' | null;
+type TextColor = 'black' | 'gray' | 'white';
+
+interface CustomProps {
+  size: TextSize;
+  weight?: TextWeight;
+  color: TextColor;
+  textStyle?: StyleProp<TextStyle>;
+}
+
+type CustomTextProps = TextProps & Partial<CustomProps>;
+
+const Text = Styled(RNText)<CustomProps>(({ theme, size, weight, color }) => {
+  const fontSize = {
+    title: theme.font.TITLE,
+    sub: theme.font.SUB,
+    base: theme.font.BASE,
+  }[size];
+
+  const fontWeight = weight === 'bold' ? 'bold' : 'normal';
+
+  const textColor = {
+    black: theme.color.BLACK,
+    gray: theme.color.GRAY[3],
+    white: theme.color.WHITE,
+  }[color];
+
+  return {
+    fontSize,
+    fontWeight,
+    color: textColor,
+  };
+});
+
+const CustomText = ({ children, size = 'base', weight, color = 'black', textStyle }: CustomTextProps) => {
+  return (
+    <Text size={size} weight={weight} color={color} style={textStyle}>
+      {children}
+    </Text>
+  );
+};
+
+export default CustomText;
