903fd0790dda0e4d022ec967b93521aa458e5305
monam2 <kangcw0107@gmail.com>
2024-09-30 02:57:27 +0900

    [FE] feat: 알바생 등록 페이지 관련 전역 상태 수정 및 추가(#87)

--- frontend/types/arbaguette.d.ts
+++ frontend/types/arbaguette.d.ts
@@ -57,6 +57,7 @@ interface SignUpForm {
 type CompanyName = string;
 type Address = string;
 type Representative = string;
+type CompanyAddress = string;
 
 interface CompanyRegisterForm {
   name: CompanyName;
