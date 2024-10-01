80e22e342f14637dbb5fe7e20e867709d0e8f7c1
황민채 <trick0846@naver.com>
2024-10-02 05:29:36 +0900

    [FE] fix: crew 뱅킹 페이지 수정

--- frontend/app/(app)/crew/authorized/banking/index.tsx
+++ frontend/app/(app)/crew/authorized/banking/index.tsx
@@ -6,45 +6,16 @@ import { Image, View } from 'react-native';
 
 import FireworkImage from '@/assets/images/firework.png';
 import PayrollImage from '@/assets/images/payroll.png';
-import WhiteLogo from '@/assets/images/white-logo.png';
-import Button from '@/components/common/Button';
+import BankingCard from '@/components/common/BankingCard';
 import CenterHeaderbar from '@/components/common/Header/CenterHeaderBar';
 import Text from '@/components/common/Text';
 import ExpectedSalaryCard from '@/components/crew/Card/ExpectedSalaryCard';
-import { useAccountBalance } from '@/reactQuery/querys';
 
 const CrewBankingScreen = () => {
-  const { accountBalance } = useAccountBalance();
-  const formatToAccountNo = (number: string): string => number.replace(/(\d{6})(\d{2})(\d+)/, '$1-$2-$3');
-  const formatToCurrency = (money: string): string => money.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
   return (
     <ScreenContainer>
       <CenterHeaderbar left="none" title="빵Pay" right="bell" />
-      <AccountSection>
-        <LogoBox>
-          <LogoImage source={WhiteLogo} />
-          <Text size="sub" weight="bold" color="white">
-            B-Pay
-          </Text>
-        </LogoBox>
-        <AccountDetailsBox>
-          <Text size="base" color="gray">
-            {accountBalance && formatToAccountNo(accountBalance?.account)}
-          </Text>
-          <Text size="title" weight="bold">
-            {accountBalance && formatToCurrency('1234567' + accountBalance?.money)} 원
-          </Text>
-          <ButtonGroup>
-            <Button textStyle={{ fontWeight: 'bold' }}>송금</Button>
-            <Button
-              textStyle={{ fontWeight: 'bold' }}
-              onPress={() => router.push('/(app)/crew/authorized/banking/transaction')}>
-              조회
-            </Button>
-          </ButtonGroup>
-        </AccountDetailsBox>
-      </AccountSection>
+      <BankingCard />
       <ActionButtonGroup>
         <ActionButton onPress={() => router.push('/crew/authorized/banking/certification')}>
           <View>
@@ -81,39 +52,6 @@ const ScreenContainer = Styled.View(({ theme }) => ({
   paddingVertical: theme.layout.PADDING.VERTICAL,
 }));
 
-const LogoImage = Styled.Image(() => ({
-  height: '100%',
-  resizeMode: 'contain',
-  width: 90,
-}));
-
-const LogoBox = Styled.View(() => ({
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  paddingVertical: 10,
-  paddingHorizontal: 10,
-  gap: 5,
-}));
-
-const AccountSection = Styled.View(({ theme }) => ({
-  width: '100%',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  backgroundColor: theme.color.PRIMARY,
-  borderRadius: theme.layout.BORDER.SECONDARY,
-  padding: 10,
-}));
-
-const AccountDetailsBox = Styled.View(({ theme }) => ({
-  flexDirection: 'column',
-  width: '100%',
-  borderRadius: theme.layout.BORDER.SECONDARY,
-  backgroundColor: 'white',
-  paddingHorizontal: 15,
-  paddingVertical: 10,
-}));
-
 const ActionButtonGroup = Styled.View(() => ({
   width: '100%',
   flexDirection: 'row',
@@ -136,11 +74,4 @@ const ActionButton = Styled.TouchableOpacity(({ theme }) => ({
   shadowOpacity: 0.6,
   shadowRadius: 6,
   elevation: 5,
-}));
-
-const ButtonGroup = Styled.View(() => ({
-  flexDirection: 'row',
-  justifyContent: 'flex-end',
-  gap: 10,
-  paddingTop: 10,
 }));
