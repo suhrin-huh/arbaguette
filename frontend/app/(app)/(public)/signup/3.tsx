b15e82f32be8b29c2cb6e6905ba1541a88532f30
황민채 <trick0846@naver.com>
2024-10-04 07:54:06 +0900

    [FE] feat: 회원가입 프로필 사진 업로드 및 로직 수정 중

--- frontend/app/(app)/(public)/signup/3.tsx
+++ frontend/app/(app)/(public)/signup/3.tsx
@@ -5,37 +5,10 @@ import type { NativeSyntheticEvent, TextInputChangeEventData } from 'react-nativ
 
 import Button from '@/components/common/Button';
 import LabeledInput from '@/components/common/LabeledInput';
-
-const Container = Styled.View(({ theme }) => ({
-  flex: 1,
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  backgroundColor: 'white',
-  paddingHorizontal: theme.layout.PADDING.HORIZONTAL,
-  paddingVertical: theme.layout.PADDING.VERTICAL,
-}));
-
-const ContentWrapper = Styled.View(() => ({
-  marginTop: 50,
-}));
-
-const InputWrapper = Styled.View(() => ({
-  marginTop: 40,
-}));
-
-const StyledTitle = Styled.Text<{ isHeader?: boolean }>(({ isHeader }) => ({
-  fontSize: isHeader ? 24 : 16,
-  fontWeight: 'bold',
-}));
-
-const ErrorText = Styled.Text(() => ({
-  color: 'red',
-  fontSize: 16,
-  marginTop: 10,
-}));
+import Text from '@/components/common/Text';
 
 const GetPasswordScreen = () => {
-  const { role, name, email } = useLocalSearchParams<{ role: 'BOSS' | 'CREW'; [key: string]: string }>();
+  const { role, profileImage, name, email } = useLocalSearchParams<{ role: 'BOSS' | 'CREW'; [key: string]: string }>();
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const [isValid, setIsValid] = useState(true);
@@ -74,13 +47,15 @@ const GetPasswordScreen = () => {
       return;
     }
 
-    router.push({ pathname: '/(app)/public/signup/4', params: { role, name, email, password } });
+    router.push({ pathname: '/(app)/(public)/signup/4', params: { role, profileImage, name, email, password } });
   };
 
   return (
     <Container>
       <ContentWrapper>
-        <StyledTitle isHeader>비밀번호를 입력해주세요.</StyledTitle>
+        <Text size="title" weight="bold">
+          비밀번호를 입력해주세요.
+        </Text>
         <InputWrapper>
           <LabeledInput
             label="비밀번호"
@@ -93,7 +68,11 @@ const GetPasswordScreen = () => {
             secureTextEntry={true}
           />
         </InputWrapper>
-        {!isValid && <ErrorText>{errorMessage}</ErrorText>}
+        {!isValid && (
+          <Text size="base" weight="bold" color="gray">
+            {errorMessage}
+          </Text>
+        )}
         <InputWrapper>
           <LabeledInput
             label="비밀번호 확인"
@@ -106,7 +85,11 @@ const GetPasswordScreen = () => {
             secureTextEntry={true}
           />
         </InputWrapper>
-        {!isConfirmed && <ErrorText>{errorMessage}</ErrorText>}
+        {!isConfirmed && (
+          <Text size="base" weight="bold" color="danger">
+            {errorMessage}
+          </Text>
+        )}
       </ContentWrapper>
       <Button type="primary" onPress={goToNext}>
         다음
@@ -114,5 +97,22 @@ const GetPasswordScreen = () => {
     </Container>
   );
 };
+
+const Container = Styled.View(({ theme }) => ({
+  flex: 1,
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  backgroundColor: 'white',
+  paddingHorizontal: theme.layout.PADDING.HORIZONTAL,
+  paddingVertical: theme.layout.PADDING.VERTICAL,
+}));
+
+const ContentWrapper = Styled.View(() => ({
+  marginTop: 50,
+}));
+
+const InputWrapper = Styled.View(() => ({
+  marginTop: 40,
+}));
 
 export default GetPasswordScreen;
---
+++ frontend/app/(app)/(public)/signup/profileImage.tsx
@@ -0,0 +1,92 @@
+import Styled from '@emotion/native';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
+
+import PlusIcon from '@/assets/images/plus.png';
+import Button from '@/components/common/Button';
+import Text from '@/components/common/Text';
+import pickImageToFormData from '@/util/boss/pickImageToFormData';
+
+const ProfileImageScreen = () => {
+  const { role } = useLocalSearchParams<{ role: 'BOSS' | 'CREW'; [key: string]: string }>();
+  const [profileImage, setProfileImage] = useState<string>('');
+  const handleGalleryPress = async () => {
+    try {
+      const result = await pickImageToFormData();
+
+      if (result) {
+        console.log(123);
+        console.log(role);
+        setProfileImage(result.uri);
+      }
+    } catch (error) {
+      console.log(123);
+    }
+  };
+
+  const goToNext = () => {
+    router.push({ pathname: '/(app)/(public)/signup/1', params: { role, profileImage } });
+  };
+
+  return (
+    <Container>
+      <ContentWrapper>
+        <Text size="title" weight="bold">
+          프로필 사진을 업로드해주세요.
+        </Text>
+        <ButtonContainer>
+          {profileImage ? (
+            <Image source={{ uri: profileImage }} />
+          ) : (
+            <CameraButton onPress={handleGalleryPress}>
+              <Icon source={PlusIcon} />
+            </CameraButton>
+          )}
+        </ButtonContainer>
+      </ContentWrapper>
+      <Button type="primary" onPress={goToNext} disabled={!profileImage}>
+        다음
+      </Button>
+    </Container>
+  );
+};
+
+export default ProfileImageScreen;
+
+const Container = Styled.View(({ theme }) => ({
+  flex: 1,
+  backgroundColor: 'white',
+  paddingHorizontal: theme.layout.PADDING.HORIZONTAL,
+  paddingVertical: theme.layout.PADDING.VERTICAL,
+}));
+
+const ContentWrapper = Styled.View(() => ({
+  flex: 1,
+  marginTop: 50,
+}));
+
+const ButtonContainer = Styled.View(({ theme }) => ({
+  flex: 1,
+  alignItems: 'center',
+  marginTop: 30,
+}));
+
+const CameraButton = Styled.Pressable(({ theme }) => ({
+  width: 200,
+  height: 200,
+  backgroundColor: theme.color.PRIMARY,
+  borderRadius: 100,
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+const Icon = Styled.Image(() => ({
+  width: 50,
+  height: 50,
+}));
+
+const Image = Styled.Image(() => ({
+  width: 200,
+  height: 200,
+  borderRadius: 100,
+}));
