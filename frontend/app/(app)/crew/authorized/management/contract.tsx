1e3d2781a531f177143f0d7ab92edd1a572eb6ad
BHyeonKim <rlaqhguse@gmail.com>
2024-10-05 15:16:33 +0900

    [FE] feat: 알바생 근로계약서 조회

--- frontend/app/(app)/crew/authorized/management/contract.tsx
+++ frontend/app/(app)/crew/authorized/management/contract.tsx
@@ -1,3 +1,22 @@
-const CrewContractPaperScreen = () => {};
+import { useLocalSearchParams } from 'expo-router';
+import Pdf from 'react-native-pdf';
+
+const CrewContractPaperScreen = () => {
+  const { url } = useLocalSearchParams<{ url: string }>();
+
+  return (
+    <Pdf
+      style={{ flex: 1 }}
+      source={{ uri: url }}
+      onLoadComplete={(numberOfPages, filePath) => {
+        console.log(`Number of pages: ${numberOfPages}`);
+      }}
+      onError={(error) => {
+        console.log(error);
+      }}
+      trustAllCerts={false}
+    />
+  );
+};
 
 export default CrewContractPaperScreen;
