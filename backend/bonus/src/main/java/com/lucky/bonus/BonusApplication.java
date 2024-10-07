d50e6ee2cce805065b6d0de386b37871fd695c6f
김지원 <gokim0928@naver.com>
2024-10-07 17:44:19 +0900

    [BE] feat: 빵 줍기 고유번호 수정

--- backend/bonus/src/main/java/com/lucky/bonus/BonusApplication.java
+++ backend/bonus/src/main/java/com/lucky/bonus/BonusApplication.java
@@ -3,10 +3,13 @@ package com.lucky.bonus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BonusApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(BonusApplication.class, args);
     }
 
