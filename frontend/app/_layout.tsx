b87977caddc405612b04a84012d0c284c545ae84
BHyeonKim <rlaqhguse@gmail.com>
2024-10-07 17:55:37 +0900

    [FE] chore: 앱아이콘 변경

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -59,20 +59,6 @@ export default function Root() {
           <GestureHandlerRootView>
             <BottomSheetModalProvider>
               <Slot />
-              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
-                <Text>Your Expo push token: {expoPushToken}</Text>
-                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                  <Text>Title: {notification && notification.request.content.title} </Text>
-                  <Text>Body: {notification && notification.request.content.body}</Text>
-                  <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-                </View>
-                <Button
-                  title="Press to Send Notification"
-                  onPress={async () => {
-                    await sendPushNotification(expoPushToken);
-                  }}
-                />
-              </View>
             </BottomSheetModalProvider>
           </GestureHandlerRootView>
         </RootLayout>
