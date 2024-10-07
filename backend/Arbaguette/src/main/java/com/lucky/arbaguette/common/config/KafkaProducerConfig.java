c8eacc9512a42ff2de5f3c2cc2db78f25c5e7aa5
박지훈 <pjh2996@naver.com>
2024-10-07 12:00:54 +0900

    [BE] config: 프로듀서 import문 수정

--- backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/config/KafkaProducerConfig.java
+++ backend/Arbaguette/src/main/java/com/lucky/arbaguette/common/config/KafkaProducerConfig.java
@@ -1,16 +1,17 @@
 package com.lucky.arbaguette.common.config;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
 import com.lucky.arbaguette.common.domain.dto.KafkaMsg;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
