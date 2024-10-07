444ea4923c97222c7ea1471f7c0f16aebcde934d
박지훈 <pjh2996@naver.com>
2024-10-07 12:06:35 +0900

    [BE] config: kafka import문 수정

--- backend/bonus/src/main/java/com/lucky/bonus/KafkaConsumerConfig.java
+++ backend/bonus/src/main/java/com/lucky/bonus/KafkaConsumerConfig.java
@@ -1,16 +1,16 @@
 package com.lucky.bonus;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConsumerConfig {
