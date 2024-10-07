d5328efe3f48c2ece0249c96fa524127e774f1c2
BHyeonKim <rlaqhguse@gmail.com>
2024-10-07 16:46:34 +0900

    [FE] feat: 클라이언트 알림 설정

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,9 +7,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { Button, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import { sendPushNotification } from '@/configs/notification';
 import usePretendardFonts from '@/hooks/usePretendardFonts';
+import usePushNotification from '@/hooks/usePushNotification';
 import Theme from '@/styles/Theme';
 
 export {
@@ -32,6 +35,7 @@ const queryClient = new QueryClient();
 
 export default function Root() {
   const [loaded, error] = usePretendardFonts();
+  const { expoPushToken, notification } = usePushNotification();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -55,6 +59,20 @@ export default function Root() {
           <GestureHandlerRootView>
             <BottomSheetModalProvider>
               <Slot />
+              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+                <Text>Your Expo push token: {expoPushToken}</Text>
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                  <Text>Title: {notification && notification.request.content.title} </Text>
+                  <Text>Body: {notification && notification.request.content.body}</Text>
+                  <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+                </View>
+                <Button
+                  title="Press to Send Notification"
+                  onPress={async () => {
+                    await sendPushNotification(expoPushToken);
+                  }}
+                />
+              </View>
             </BottomSheetModalProvider>
           </GestureHandlerRootView>
         </RootLayout>
---
+++ frontend/hooks/usePushNotification.tsx
@@ -0,0 +1,34 @@
+import * as Notifications from 'expo-notifications';
+import { useEffect, useRef, useState } from 'react';
+
+import { registerForPushNotificationsAsync } from '@/configs/notification';
+
+const usePushNotification = () => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.current && Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current && Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return { expoPushToken, notification };
+};
+
+export default usePushNotification;
