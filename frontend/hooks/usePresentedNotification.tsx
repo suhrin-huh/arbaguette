c7c5b2694c32e6b3adc0d162a952686e32bbd4bd
BHyeonKim <rlaqhguse@gmail.com>
2024-10-08 14:33:11 +0900

    [FE] feat: 알림 내역 리스트 리패치 (#115)

--- frontend/hooks/usePresentedNotification.tsx
+++ frontend/hooks/usePresentedNotification.tsx
@@ -3,10 +3,13 @@ import { getPresentedNotificationsAsync } from 'expo-notifications';
 
 import keys from '@/reactQuery/keys';
 
+const MINUTE = 1000;
+
 const usePresentedNotification = () => {
   const { data } = useQuery({
     queryKey: [keys.all, 'notification'],
     queryFn: getPresentedNotificationsAsync,
+    refetchInterval: MINUTE * 3,
   });
 
   return data || [];
